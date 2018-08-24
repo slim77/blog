19536daaeb20a338d93933b64aa50ce6e65729dd
slim77 <35788843+slim77@users.noreply.github.com>
2018-08-24 15:22:29 +0000

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php 
+define("ROOT", direname(__DIR__));
+require ROOT."/app/Autoload.php";
+
+if(isset($_GET['p']))
+{
+  $p = $_GET['p'];
+}else{
+  $p = 'home';
+}
+
+ob_start();
+
+if($p === 'home'){
+  require ROOT.'/pages/posts/home.php';
+}
+
+$content = ob_get_clean();
+require ROOT.'/pages/tamplate/default.php';
