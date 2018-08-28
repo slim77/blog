638208bb3a0564d6e827285734bf6a3ec4178e9c
slim77 <35788843+slim77@users.noreply.github.com>
2018-08-28 19:02:46 +0000

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,11 @@
 <h1> Bienvenue sur mon blog </h1>
+<div class="row">
+  <div class="col-sm-8">
+    <?php foreach(App::getTable('Post')->last() as $posts): ?>
+    <h1><a href="<?= $post->url; ?>"><?= $posts->title; ?></a></h1>
+    <em><?= $posts->categorie; ?></em>
+    <p><?= $posts->content?></p>
+    <?php endforeach; ?>
+  </div>
+  <div class="col-sm-4"></div>
+</div>
